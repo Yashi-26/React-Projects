af0e16144c0aa2223e48dea2c32740e32d2f3bab
Yashi-26 <yashika7991@gmail.com>
2022-07-29 17:46:05 +0530

    ExpenseItem1

---
+++ src/component/ExpenseItem.js
@@ -0,0 +1,19 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>
+        <h2>Food</h2>
+      </div>
+      <div>Rs 10</div>
+      <div>
+        <h2>Petrol</h2>
+      </div>
+      <div>Rs 100</div>
+      <div>
+        <h2>Movies</h2>
+      </div>
+      <div>Rs 200</div>
+    </div>
+  );
+}
+export default ExpenseItem;
