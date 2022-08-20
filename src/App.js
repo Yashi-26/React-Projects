a9143abe98023b59aa5a77619518c4b96e4f15d9
Yashi-26 <yashika7991@gmail.com>
2022-08-20 21:14:50 +0530

    onChange

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NewExpense from './components/NewExpense/NewExpense';
 
 import Expenses from './components/Expenses/Expenses';
 
@@ -37,7 +38,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
